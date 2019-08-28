ce75236c8d9352751013944e625caf6034abfd80
animation <763678968@qq.com>
2019-08-28 20:50:14 +0800

    基于静态内部类的单例模式实现

---
+++ src/StaticHolderSingleton.java
@@ -0,0 +1,27 @@
+import stf.Debug;
+
+public class StaticHolderSingleton {
+    // 私有构造器
+    private StaticHolderSingleton() {
+        Debug.info("StaticHolderSingleton inited.");
+    }
+
+    private static class InstanceHolder {
+        // 保存外部类的唯一实例
+        final static StaticHolderSingleton INSTANCE = new StaticHolderSingleton();
+    }
+
+    public static StaticHolderSingleton getInstance() {
+        Debug.info("getInstance invoked.");
+        return InstanceHolder.INSTANCE;
+    }
+
+    public void someService() {
+        Debug.info("someService invoked.");
+        // 省略其他代码
+    }
+
+    public static void main(String[] args) {
+        StaticHolderSingleton.getInstance().someService();
+    }
+}
