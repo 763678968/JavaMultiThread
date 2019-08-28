dd8602f92663d9277d310a147dae7726960a2036
animation <763678968@qq.com>
2019-08-28 19:14:38 +0800

    基于volatile的简易读写锁

---
+++ src/Counter.java
@@ -0,0 +1,11 @@
+public class Counter {
+    private volatile long count;
+    public long value() {
+        return count;
+    }
+    public void increment() {
+        synchronized (this) {
+            count++;
+        }
+    }
+}
