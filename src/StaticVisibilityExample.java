2fe2c218ddd61e90a689511c77241c55fc2452d2
animation <763678968@qq.com>
2019-08-29 10:40:03 +0800

    static关键字可见性保障示例

---
+++ src/StaticVisibilityExample.java
@@ -0,0 +1,41 @@
+import stf.Debug;
+import stf.Tools;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StaticVisibilityExample {
+    private static Map<String, String> taskConfig;
+    static {
+        Debug.info("The class being initialized...");
+        taskConfig = new HashMap<String, String>(); // 语句①
+        taskConfig.put("url", "https://github.com/763678968"); // 语句②
+        taskConfig.put("timeout", "1000"); // 语句③
+    }
+
+    public static void changeConfig(String url, int timeout) {
+        taskConfig = new HashMap<String, String>(); // 语句④
+        taskConfig.put("url", url); // 语句⑤
+        taskConfig.put("timeout", String.valueOf(timeout)); // 语句⑥
+    }
+
+    public static void init() {
+        // 该线程至少能够看到语句①~语句③的操作结果，而能否看到语句④~语句⑥的操作结果是没有保障的
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                String url = taskConfig.get("url");
+                String timeout = taskConfig.get("timeout");
+                doTask(url, Integer.valueOf(timeout));
+            }
+        };
+        t.start();
+    }
+
+    private static void doTask(String url, int timeout) {
+        // 省略其他代码
+
+        // 省略实际操作的耗时
+        Tools.randomPause(500);
+    }
+}
