9b9bfadded29b606a45e757190b0dac2f51377a9
animation <763678968@qq.com>
2019-08-29 10:33:01 +0800

    类的延迟初始化

---
+++ src/ClassLazyInitDemo.java
@@ -0,0 +1,16 @@
+import stf.Debug;
+
+public class ClassLazyInitDemo {
+    public static void main(String[] args) {
+        Debug.info(Collaborator.class.hashCode()); // 语句①
+        Debug.info(Collaborator.number); // 语句②
+        Debug.info(Collaborator.flag);
+    }
+    static class Collaborator {
+        static int number = 1;
+        static boolean flag = true;
+        static {
+            Debug.info("Collaborator initializing...");
+        }
+    }
+}
