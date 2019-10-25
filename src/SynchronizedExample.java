b24e2113ab7b71fec6ede92d05eff8450e1d247e
animation <763678968@qq.com>
2019-10-25 10:13:53 +0800

    synchronized 同步一个类

--- src/SynchronizedExample.java
+++ src/SynchronizedExample.java
@@ -3,8 +3,8 @@ import java.util.concurrent.Executors;
 
 public class SynchronizedExample {
 
-    public void func1() {
-        synchronized (this) {
+    public void func2() {
+        synchronized (SynchronizedExample.class) {
             for (int i = 0; i < 10; i++) {
                 System.out.println(i + " ");
             }
@@ -15,7 +15,7 @@ public class SynchronizedExample {
         SynchronizedExample e1 = new SynchronizedExample();
         SynchronizedExample e2 = new SynchronizedExample();
         ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.execute(() -> e1.func1());
-        executorService.execute(() -> e2.func1());
+        executorService.execute(() -> e1.func2());
+        executorService.execute(() -> e2.func2());
     }
 }
